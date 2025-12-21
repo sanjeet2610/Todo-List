2a50c71562dd432d17eda484384561729d206856
sanjeet2610 <195545915+sanjeet2610@users.noreply.github.com>
2025-12-21 16:15:39 +0000

    Add dom for project list in sidebar

---
+++ src/dom/projectDom.js
@@ -0,0 +1,22 @@
+function renderProjects(manager) {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.textContent = "";
+  const allProjects = manager.getAllProjects();
+
+  allProjects.forEach((project) => {
+    const name = project.getProjectName();
+    const projectElement = document.createElement("p");
+    projectElement.textContent = name;
+    projectElement.dataset.id = project.getProjectId();
+    sidebar.appendChild(projectElement);
+    if (manager.getCurrentProject().getProjectId() === project.getProjectId()) {
+      projectElement.style.backgroundColor = "red";
+    }
+    projectElement.addEventListener("click", (e) => {
+      manager.setCurrentProject(project.getProjectId());
+      renderProjects(manager);
+    });
+  });
+}
+
+export { renderProjects };
